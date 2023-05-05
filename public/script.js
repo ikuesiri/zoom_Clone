362b20a807290930e8a476383365c9911d7eef8b
Ikuesiri Onosemuode <ikuesiri.onosemuode@gmail.com>
2023-05-05 07:14:05 +0100

    adding and setting up of socket.io npm

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
-
+const socket = io();
  let videoBase = document.querySelector("#video-base");
 const myVideo  = document.createElement("video");
+// const myAudio  = document.createElement("audio");
 let myVideoStream;
 
 /**
@@ -14,13 +15,34 @@ navigator.mediaDevices.getUserMedia({
  // selecting "true" gives access to the video & audio
 }).then( stream => {
     myVideoStream = stream;
-    addVideoStream(myVideo, stream)
+    addVideoStream(myVideo, stream);
+    // addAudioStream( myAudio, stream);
+})
+.catch( (err) =>{
+    console.log(`Error accessing media : ${err}`);
 })
 
+ // This emits an event to the server , which the server socket.on withe the paramter "join-room", listens to and emits a response
+    socket.emit("join-room", ROOM_ID);
+    //socket "on" method listens to the event form the server side
+   socket.on("user-connected", ()=>{
+    newlyConnectedUser()
+   })
+   newlyConnectedUser =()=>{
+     console.log(" Welcome our new User!!!!!!")
+   }
+
+//FUNCTIONS
+//Display the user's video from stream in a video element
 const addVideoStream =(video, stream) =>{
     video.srcObject = stream;
     video.addEventListener("loadedmetadata", () =>{
         video.play();
     })
     videoBase.append(video);
+}
+
+const addAudioStream = (audio, stream) =>{
+    audio.srcObject = stream;
+    audio.play();
 }
--- server.js
+++ server.js
@@ -2,15 +2,14 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const { v4: uuidv4 } = require('uuid');
+const io = require("socket.io")(server);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) =>{
     res.redirect(`/${uuidv4()}`)
-   
 })
-
 
 app.get("/:roomId", (req, res) =>{
     const roomId =  req.params.roomId
@@ -18,6 +17,18 @@ app.get("/:roomId", (req, res) =>{
         roomId
     })
 })
+
+io.on("connection", socket => {
+    //socket "on" method listens to the event form the client side
+    socket.on("join-room", (roomId)=>{
+        socket.join(roomId); // this will add the new user to the "roomId" room.
+        socket.broadcast.to(roomId).emit("user-connected"); 
+        // This emits an event to all users in the room that a new user has joined 
+        //(But the new Member doesn't see this event)
+    })
+})
+
+
 
 PORT =  process.env.PORT || 3000;
 server.listen(PORT, ()=> console.log(`Servering listening at port:${PORT}`));
